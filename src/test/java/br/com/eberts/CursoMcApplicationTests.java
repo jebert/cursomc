245e9d4ac4b8b28fcceda116e67001bc62dc838d
Jonatan Ebert <jonatan.ebert@gmail.com>
2023-02-02 23:08:28 -0300

    First commit, project created

---
+++ src/test/java/br/com/eberts/CursoMcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eberts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
