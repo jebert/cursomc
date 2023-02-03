730a31bca588ac0ce91cac1920f33a89188114db
Jonatan Ebert <jonatan.ebert@gmail.com>
2023-02-03 00:19:20 -0300

    Database Postgres and autocreate

--- src/main/java/br/com/eberts/domain/Categoria.java
+++ src/main/java/br/com/eberts/domain/Categoria.java
@@ -2,6 +2,10 @@ package br.com.eberts.domain;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -13,10 +17,13 @@ import lombok.ToString;
 @NoArgsConstructor
 @EqualsAndHashCode
 @ToString
+@Entity
 public class Categoria implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
--- src/main/java/br/com/eberts/resources/CategoriaResource.java
+++ src/main/java/br/com/eberts/resources/CategoriaResource.java
@@ -1,7 +1,6 @@
 package br.com.eberts.resources;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
